7d499de2fd216e067a39b5c271f15a8d49f412c6
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2022-01-10 09:00:10 +0900

    refactor: use utils

--- SymbolicFiniteDifference.py
+++ SymbolicFiniteDifference.py
@@ -1,35 +1,13 @@
 import sympy as sp
-
-_DefaultIndependentVariableSymbolStr = 'x'
-_DefaultIntervalSymbolStr = 'h'
-_DefaultFunctionSymbolStr = 'f'
-
-
-def createXSetFromStencil(stencil, intervalSymbolStr=_DefaultIntervalSymbolStr):
-    return [stencil[i]*sp.symbols(intervalSymbolStr) for i in range(len(stencil))]
-
-
-def createSetOfFunctionSymbolsAtXSet(xSet, functionSymbolStr=_DefaultFunctionSymbolStr,
-                                     sameSubscriptsAsStencil=False):
-
-    if sameSubscriptsAsStencil:
-        stencil = [x if x.is_number else sp.poly(x).coeffs()[0] for x in xSet]
-        subscript = ['_{%d}' % i if i.is_integer else '_{%2.1f}' % i
-                     for i in stencil]
-        str = ''.join([functionSymbolStr + s + ' ' for s in subscript])
-        fSet = sp.symbols(str)
-    else:
-        fSet = sp.symbols(functionSymbolStr+'_0:{:d}'.format(len(xSet)))
-
-    return fSet
+import utils as util
 
 
 def getFiniteDifferenceEquation(stencil, orderOfDifference=1,
-                                intervalSymbolStr=_DefaultIntervalSymbolStr,
+                                intervalSymbolStr=util._DefaultIntervalSymbolStr,
                                 sameSubscriptsAsStencil=False):
-    xSet = createXSetFromStencil(stencil, intervalSymbolStr)
-    fSet = createSetOfFunctionSymbolsAtXSet(xSet, _DefaultFunctionSymbolStr,
-                                            sameSubscriptsAsStencil)
+    xSet = util.createXSetFromStencil(stencil, intervalSymbolStr)
+    fSet = util.createSetOfFunctionSymbolsAtXSet(xSet, util._DefaultFunctionSymbolStr,
+                                                 sameSubscriptsAsStencil)
 
     coef = getFiniteDifferenceCoefficients(stencil, orderOfDifference)
 
@@ -40,10 +18,11 @@ def getFiniteDifferenceEquation(stencil, orderOfDifference=1,
 def getFiniteDifferenceCoefficients(stencil, orderOfDifference=1, as_numr_denom=False):
     import lagrangianpoly as lp
 
-    xSet = createXSetFromStencil(stencil, _DefaultIntervalSymbolStr)
-    fSet = createSetOfFunctionSymbolsAtXSet(xSet, _DefaultFunctionSymbolStr)
+    xSet = util.createXSetFromStencil(stencil, util._DefaultIntervalSymbolStr)
+    fSet = util.createSetOfFunctionSymbolsAtXSet(
+        xSet, util._DefaultFunctionSymbolStr)
 
-    x = sp.symbols(_DefaultIndependentVariableSymbolStr)
+    x = sp.symbols(util._DefaultIndependentVariableSymbolStr)
     num, den = lp.LagrangianPoly(x, xSet, fSet).as_numer_denom()
     num_coef = num.as_poly(fSet).coeffs()
     den_coef = den.as_poly(fSet).coeffs()
@@ -51,18 +30,4 @@ def getFiniteDifferenceCoefficients(stencil, orderOfDifference=1, as_numr_denom=
     coef = [lp.Derivative(num/den_coef[0], x, orderOfDifference)
             for num in num_coef]
 
-    coef_num = [c if c.is_number else sp.poly(c).coeffs()[0] for c in coef]
-
-    import numpy as np
-    import fractions as fr
-    coef_rational = [sp.Rational(fr.Fraction(
-        str(c)).limit_denominator(100000)) for c in coef_num]
-
-    denom = [c.q for c in coef_rational]
-    denom_lcm = np.lcm.reduce(np.array(denom))
-    numr = [c*denom_lcm for c in coef_rational]
-
-    if as_numr_denom:
-        return numr, denom_lcm
-    else:
-        return [n/denom_lcm for n in numr]
+    return util.simplifyCoefficients(coef, as_numr_denom)
